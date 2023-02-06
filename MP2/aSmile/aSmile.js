57e5d4eead39b56566d0a13b9c9121015c312224
ebeberness <enb1@uw.edu>
2023-02-05 21:06:49 -0800

    adding to write up, making final image adjustments

--- MP2/aSmile/aSmile.js
+++ MP2/aSmile/aSmile.js
@@ -7,8 +7,8 @@ function setup() {
     fill("yellow");
     circle(windowWidth/2, windowHeight/2, 500);
     fill(0);
-    square(windowWidth/2.6, windowHeight/2.6, 50);
-    square(windowWidth/1.75, windowHeight/2.6, 50);
+    square(windowWidth/2.45, windowHeight/2.6, 50);
+    square(windowWidth/1.8, windowHeight/2.6, 50);
     //noFill();
     arc(windowWidth/2, windowHeight/1.5, 90, 75, 0, PI);
     
